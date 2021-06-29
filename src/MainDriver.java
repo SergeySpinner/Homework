b5ec56e9976c597b607034d77a9fa9e7740b788e
SergeySpinner <71096115+SergeySpinner@users.noreply.github.com>
2021-06-29 13:35:39 +0300

    Delete src directory

--- src/MainDriver.java
+++
@@ -1,7 +0,0 @@
-public class MainDriver {
-    public static void main(String[] args) {
-        IceMaker iceMaker = new IceMaker(new StrawberryDecorator(new VanillaDecorator(new CoconutDecorator(null))));
-
-        System.out.println(iceMaker.make());
-    }
-}
